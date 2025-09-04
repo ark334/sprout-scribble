025076a1966a96313fcdc2e08935ba1a83a1e645
ark334 <ark333@gmail.com>
2025-09-04 15:32:50 +0100

    Initial

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function About() {
+    return(
+        <div>
+            <h1>About</h1>
+            <p>About project page</p>
+            <Link href="/">Back home</Link>
+        </div>
+    )
+}
